12cb5bb1f75b3068738cfbd011ccfbefc1088d91
puguhrismadi <puguh.rismadi@gmail.com>
2020-06-11 17:26:01 +0700

    projek bot telegram dengan python part 3 koneksi dan baca data dari mysql

--- Mybot.py
+++ Mybot.py
@@ -1,12 +1,14 @@
 import telebot
-import mysql
+import mysql.connector
 
 import mytoken
 
-from datetime import date
+
 from datetime import datetime
 TOKEN=mytoken.TOKEN
 myBot = telebot.TeleBot(TOKEN)
+myDb=mysql.connector.connect(host='localhost',user='root',database='db_belajarbot')
+sql=myDb.cursor()
 from telebot import apihelper
 waktusekarang=datetime.now()
 
@@ -21,5 +23,30 @@ class Mybot:
         teks = mytoken.SAPA + "\n-- admin & developer @puguhrismadi - SMK Taruna Bhakti -- "+"\n" \
                         "hari ini tanggal "+str(waktusekarang)
         myBot.reply_to(message, teks)
+
+    @myBot.message_handler(commands=['datasiswa'])
+    def menu_data_siswa(message):
+        query="select nipd,nama,kelas from tabel_siswa "
+        sql.execute(query)
+        data=sql.fetchall()
+        jmldata = sql.rowcount
+        kumpuldata=''
+        if(jmldata>0):
+            #print(data)
+            no=0
+            for x in data:
+                no += 1
+                kumpuldata =kumpuldata+ str(x)
+                print(kumpuldata)
+                kumpuldata = kumpuldata.replace('(', '')
+                kumpuldata = kumpuldata.replace(')', '')
+                kumpuldata = kumpuldata.replace("'", '')
+                kumpuldata = kumpuldata.replace(",", '')
+        else:
+            print('data kosong')
+
+        myBot.reply_to(message,str(kumpuldata))
+
+print(myDb)
 print("-- Bot sedang berjalan --")
 myBot.polling(none_stop=True)
